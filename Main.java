876c314f3c9280cae511a0aced4cd9989c948c0a
qurek9038 <qurek9038@wrdsb.ca>
2020-10-13 08:13:35 +0000

    While Loop Test Question Completed

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *This program prompts the user to enter in a positive integer. It will then calculate the sum of all of the digits in that integer
+ * @author Khazina Qureshi
  */
 public class Main {
 
@@ -9,7 +10,30 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    //Create the scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //Ask the user for a positive integer
+    System.out.println("Please enter a positive integer to add the digits of:");
+
+    //Store user input using variable 
+    int positiveNum = input.nextInt();
+
+    //Check to make sure it is a positive integer
+    while (positiveNum <= 0){
+      //Ask again for a positive integer if user entered a negative integer
+      System.out.println("That number is not a positive integer. Please try again!");
+      positiveNum = input.nextInt();
+    }
+    //Pre looping code
+    int sum = 0;
+    while (positiveNum > 0){
+      //Pull integer into different pieces using modulus and division operations in the loop
+      sum = sum + positiveNum % 10;
+      positiveNum = positiveNum / 10;
+    }
+    //Display the sum of the digits to user 
+    System.out.println("The sum of these digits is " + sum);
+ 
   }
 }
